5d6abc03e6e80257a7e1641574a4b0757096ba53
lidor115 <lidor115@gmail.com>
2019-06-14 15:54:28 +0300

    connect

---
+++ app/src/main/java/Connect.java
@@ -0,0 +1,33 @@
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Connect {
+    private String ip;
+    private int port;
+    private Thread t;
+
+    public Connect(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public void connection() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InetAddress serverAddr = InetAddress.getByName(ip);
+                    Socket socket = new Socket(serverAddr, port);
+                } catch (IOException e) {
+                    Log.e("TCP", "C: Error", e);
+                    System.out.println((e.toString()));
+                }
+            }
+        };
+        this.t = new Thread(r);
+        this.t.start();
+    }
+}
